6c98097802187da4838796155c23cc9af8fb1762
AwadYMohammed <awadyasirawad@gmail.com>
2020-10-22 03:09:32 -0400

    Adding add-student method

--- src/com/awad/web/jdbc/StudentDbUtil.java
+++ src/com/awad/web/jdbc/StudentDbUtil.java
@@ -1,4 +1,4 @@
-package com.awad.web.jdbc;
+package com.luv2code.web.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -81,6 +81,35 @@ public class StudentDbUtil {
 		}
 	}
 
+	public void addStudent(Student theStudent) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+			
+			// create sql for insert
+			String sql = "insert into student "
+					   + "(first_name, last_name, email) "
+					   + "values (?, ?, ?)";
+			
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set the param values for the student
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			
+			// execute sql insert
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}
+	}
 }
 
 
