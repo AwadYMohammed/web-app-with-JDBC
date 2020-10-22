c8b96c9a856ed5e0d7b4a493d4ffa07b0a26c824
AwadYMohammed <awadyasirawad@gmail.com>
2020-10-22 03:37:21 -0400

    Adding UPDATE functionality feature

--- src/com/awad/web/jdbc/StudentControllerServlet.java
+++ src/com/awad/web/jdbc/StudentControllerServlet.java
@@ -61,6 +61,9 @@ public class StudentControllerServlet extends HttpServlet {
 			case "LOAD":
 				loadStudent(request, response);
 				break;
+			case "UPDATE":
+				updateStudent(request, response);
+				break;
 				
 			default:
 				listStudents(request, response);
@@ -73,6 +76,27 @@ public class StudentControllerServlet extends HttpServlet {
 		
 	}
 
+
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+			// read student info from form data
+			int id = Integer.parseInt(request.getParameter("studentId"));
+			String firstName = request.getParameter("firstName");
+			String lastName = request.getParameter("lastName");
+			String email = request.getParameter("email");
+			
+			// create a new student object
+			Student theStudent = new Student(id, firstName, lastName, email);
+			
+			// perform update on database
+			studentDbUtil.updateStudent(theStudent);
+			
+			// send them back to the "list students" page
+			listStudents(request, response);
+			
+		}
 
 
 	private void loadStudent(HttpServletRequest request, HttpServletResponse response) 
