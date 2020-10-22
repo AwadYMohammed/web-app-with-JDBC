db6f1a3b0d8a2f8767dd92861621a86f577ac52f
AwadYMohammed <awadyasirawad@gmail.com>
2020-10-22 03:26:31 -0400

    Prepopulating the HTML form

--- src/com/awad/web/jdbc/StudentControllerServlet.java
+++ src/com/awad/web/jdbc/StudentControllerServlet.java
@@ -58,6 +58,9 @@ public class StudentControllerServlet extends HttpServlet {
 			case "ADD":
 				addStudent(request, response);
 				break;
+			case "LOAD":
+				loadStudent(request, response);
+				break;
 				
 			default:
 				listStudents(request, response);
@@ -71,6 +74,24 @@ public class StudentControllerServlet extends HttpServlet {
 	}
 
 
+
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+
+			// read student id from form data
+			String theStudentId = request.getParameter("studentId");
+			
+			// get student from database (db util)
+			Student theStudent = studentDbUtil.getStudent(theStudentId);
+			
+			// place student in the request attribute
+			request.setAttribute("THE_STUDENT", theStudent);
+			
+			// send to jsp page: update-student-form.jsp
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("/update-student-form.jsp");
+			dispatcher.forward(request, response);		
+		}
 
 	private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
